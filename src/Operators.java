99648bfb306a650359323dcdebe45e8c2a670fc1
Karan Singh <veerkaransinghmgm@gmail.com>
2025-06-10 22:32:44 -0400

    Initial commit - Zoo Management System

--- src/Operators.java
+++ src/Operators.java
@@ -1,2 +1,16 @@
 public class Operators {
+
+    public static void main(String [] args){
+        System.out.println ("Operators review");
+
+        int totalAmount = 234; //Camel case convention for totalAmount
+        int TotalAmount = 134; //Java is a case sensitive language, so totalAmount and TotalAmount are
+                               // different
+
+        System.out.println(totalAmount + TotalAmount);
+        System.out.println(totalAmount - TotalAmount);
+        System.out.println(totalAmount < TotalAmount); //boolean output for relational operator
+
+
+    }
 }
