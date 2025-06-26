fee3a5b6d918607f964b0c972ae35b55737b01f0
Karan Singh <veerkaransinghmgm@gmail.com>
2025-06-26 16:51:48 -0400

    Initial commit - Zoo Management System

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println(" Mustang is my favourite carrrr");
+
         System.out.println("Hello world!");
     }
 }
