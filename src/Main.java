9b31a86178aa8ea5183d896ddbaacab51aff913a
Karan Singh <veerkaransinghmgm@gmail.com>
2025-07-01 23:29:59 -0400

    Initial commit - Zoo Management System

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        int a = 34;
+        int b = 34;
+        System.out.println(a +b);
+
         System.out.println(" Mustang is my favourite carrrr");
         System.out.println("I wish i can _____");
         System.out.println(" How difficult is it to get a job");
+        System.out.println(" July 1 ");
+
 
         System.out.println("Hello world!");
     }
